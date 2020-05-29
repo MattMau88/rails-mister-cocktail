035f6e7b51ca78ed4d118c6d006beadc0c30105e
Matthias Mau <matthias.mau@outlook.com>
2020-05-29 08:17:59 +0300

    commit

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy # destroy doses but not ingredients
   has_many :ingredients, through: :doses
 
+  has_one_attached :photo
+
   validates :name, presence: true, uniqueness: true
 end
